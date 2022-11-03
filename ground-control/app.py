4bb6295ed5914e6992d205ff416569beaa21923a
DersuG <dersu.goorskey@gmail.com>
2022-11-02 20:21:24 -0400

    create program starting point

---
+++ ground-control/app.py
@@ -0,0 +1,40 @@
+import dearpygui.dearpygui as gui
+import serial
+from serial_test.iliad_data_controller import IliadDataController
+
+class App:
+    def __init__(self) -> None:
+        # Start UI:
+        gui.create_context()
+        gui.create_viewport(title='Iliad Ground Control', width=600, height=300)
+        gui.setup_dearpygui()
+
+        with gui.window() as MAIN_WINDOW:
+            gui.set_primary_window(MAIN_WINDOW, True)
+
+        gui.show_viewport()
+        gui.maximize_viewport()
+
+        # Start serial ports and stuff:
+        self.iliad = IliadDataController()
+        self.config = {
+            'port_name': 'COM2',
+            'port_baud_rate': 9600,
+            'port_stop_bits': serial.STOPBITS_ONE,
+            'port_parity': serial.PARITY_NONE,
+            'port_byte_size': serial.EIGHTBITS,
+        }
+        self.iliad.set_config(self.config)
+        self.iliad.open()
+
+    def update(self) -> None:
+        self.iliad.update()
+
+    def run(self) -> None:
+        while gui.is_dearpygui_running():
+            self.update()
+            gui.render_dearpygui_frame()
+
+        self.iliad.close()
+        
+        gui.destroy_context()
--- ground-control/main.py
+++ ground-control/main.py
@@ -1,22 +1,11 @@
-import serial
-import serial.tools.list_ports
-import struct
-import utils.packet_util
-import crc
-from serial_test.iliad_data_controller import IliadDataController
+import dearpygui.dearpygui as gui
+from app import App
 
-def list_serial_ports():
-    for device in serial.tools.list_ports.comports():
-        print(f'{device.name}\t{device.description}')
+def update():
+    pass
 
-iliad = IliadDataController()
-config = {
-    'port_name': 'COM2',
-    'port_baud_rate': 9600,
-    'port_stop_bits': serial.STOPBITS_ONE,
-    'port_parity': serial.PARITY_NONE,
-    'port_byte_size': serial.EIGHTBITS,
-}
-iliad.set_config(config)
-iliad.open()
-iliad.close()
+if __name__ == '__main__':
+
+    app = App()
+    app.run()
+    
