616f9d5f93b16a42c3a92306182241e0c237a6a4
DersuG <dersu.goorskey@gmail.com>
2023-04-18 20:54:33 -0400

    clean up

--- ground-control/tests/packet_util_test.py
+++
@@ -1,9 +0,0 @@
-import packetlib.packet as packet
-
-def test_get_packet_types():
-    assert list(packet.get_packet_types(
-        packet.PACKET_TYPE_ACCELERATION + packet.PACKET_TYPE_ALTITUDE
-    )) == [
-        packet.PACKET_TYPE_ALTITUDE,
-        packet.PACKET_TYPE_ACCELERATION,
-    ]
