2e756fcc66a6f29a53c9dd7a3c5c4d3635f718c0
Brian Jia <brian.jia04@gmail.com>
2024-11-18 01:35:44 -0500

    Serial connection: using threads now!

--- ground-control/src/main.rs
+++ ground-control/src/main.rs
@@ -3,13 +3,13 @@
 mod data;
 mod live_tab;
 mod plot_tab;
-mod trajectory_tab;
 mod serial_connection;
+mod trajectory_tab;
 
 use data::Data;
 use live_tab::LiveTab;
 use rand;
-use serial_connection::SerialConnection;
+use serial_connection::{Status, SerialConnection};
 use std::io::BufRead;
 use trajectory_tab::TrajectoryTab;
 
@@ -57,18 +57,7 @@ struct GroundControlApp {
     data: data::Data,
     data_t: f64,
 
-    serialconnection: SerialConnection,
-
-    // TODO: clean up relationship between activeport and selectedport
-    // TODO: clean up representation of port status (ConnectionStatus vs. Option vs. Result, etc.)
-    serialport_knownports: Vec<SerialPortInfo>,
-    serialport_selectedport: String, // the name of the port selected in the ui, "" if not connected
-    serialport_baudrate: u32,
-    serialport_databits: serialport::DataBits,
-    serialport_parity: serialport::Parity,
-    serialport_stopbits: serialport::StopBits,
-
-    serialport_messages: Vec<String>,
+    serial: SerialConnection,
 
     ui_showsidebar: bool,
     ui_selectedtab: AppTab,
@@ -86,43 +75,37 @@ impl GroundControlApp {
         //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         // }
 
-        Default::default()
-    }
+        let mut app = Self {
+            tab_plot: PlotTab::new(),
+            tab_live: LiveTab::new(),
+            tab_trajectory: TrajectoryTab::new(),
 
-    fn serialport_connect(&mut self) {
-        self.serialconnection.connect(
-            self.serialport_selectedport.clone(),
-            self.serialport_baudrate,
-            self.serialport_databits,
-            self.serialport_parity,
-            self.serialport_stopbits,
-            100,
-        );
+            data: Data::new(),
+            data_t: 0.0,
+
+            serial: serial_connection::SerialConnection::new(),
+
+            ui_showsidebar: true,
+            ui_selectedtab: AppTab::Plot,
+        };
+
+        app.serial.refresh_known_ports();
+
+        app
     }
 
     fn serialport_disconnect(&mut self) {
-        self.serialconnection.disconnect();
+        self.serial.disconnect();
     }
 
     fn ui_add_serialportui(&mut self, ui: &mut egui::Ui) {
         // let mut settings_isenabled = false; // port name, baud rate, etc.
         // let mut connect_isenabled = false; // connect button
         // let mut disconnect_isenabled = true; // disconnect button
-        let isconnected = self.serialconnection.isconnected();
-        let settings_isenabled = !isconnected;
-        let connect_isenabled = !isconnected && !self.serialport_selectedport.is_empty();
-        let disconnect_isenabled = isconnected;
-        // match &app.serialport_activeport {
-        //     Err(e) => match e.kind {
-        //         serialport::ErrorKind::NoDevice => {
-        //             settings_isenabled = true;
-        //             connect_isenabled = true;
-        //             disconnect_isenabled = false;
-        //         },
-        //         _ => {},
-        //     },
-        //     _ => {},
-        // }
+        let settings_isenabled = self.serial.connection_allowed();
+        let connect_isenabled =
+            self.serial.connection_allowed() && !self.serial.selected_port.is_empty();
+        let disconnect_isenabled = self.serial.disconnection_allowed();
 
         // port settings
         ui.add_enabled_ui(settings_isenabled, |ui| {
@@ -130,49 +113,47 @@ impl GroundControlApp {
             ui.horizontal(|ui| {
                 GroundControlApp::ui_draw_serialportdropdown(
                     ui,
-                    &mut self.serialport_knownports,
-                    &mut self.serialport_selectedport,
+                    &mut self.serial.known_ports,
+                    &mut self.serial.selected_port,
                 );
                 ui.label("Port");
                 if ui.button("Refresh").clicked() {
-                    self.serialport_knownports =
-                        serialport::available_ports().unwrap_or(Vec::new());
-                    // TODO: handle this error properly
+                    self.serial.refresh_known_ports();
                 }
             });
 
             // baud rate
             egui::ComboBox::from_label("Baud rate")
-                .selected_text(format!("{}", self.serialport_baudrate))
+                .selected_text(format!("{}", self.serial.baud_rate))
                 .show_ui(ui, |ui| {
-                    ui.selectable_value(&mut self.serialport_baudrate, 50, "50");
-                    ui.selectable_value(&mut self.serialport_baudrate, 75, "75");
-                    ui.selectable_value(&mut self.serialport_baudrate, 110, "110");
-                    ui.selectable_value(&mut self.serialport_baudrate, 134, "134");
-                    ui.selectable_value(&mut self.serialport_baudrate, 150, "150");
-                    ui.selectable_value(&mut self.serialport_baudrate, 200, "200");
-                    ui.selectable_value(&mut self.serialport_baudrate, 300, "300");
-                    ui.selectable_value(&mut self.serialport_baudrate, 600, "600");
-                    ui.selectable_value(&mut self.serialport_baudrate, 1200, "1200");
-                    ui.selectable_value(&mut self.serialport_baudrate, 1800, "1800");
-                    ui.selectable_value(&mut self.serialport_baudrate, 2400, "2400");
-                    ui.selectable_value(&mut self.serialport_baudrate, 4800, "4800");
-                    ui.selectable_value(&mut self.serialport_baudrate, 9600, "9600");
-                    ui.selectable_value(&mut self.serialport_baudrate, 19200, "19200");
-                    ui.selectable_value(&mut self.serialport_baudrate, 28800, "28800");
-                    ui.selectable_value(&mut self.serialport_baudrate, 38400, "38400");
-                    ui.selectable_value(&mut self.serialport_baudrate, 57600, "57600");
-                    ui.selectable_value(&mut self.serialport_baudrate, 76800, "76800");
-                    ui.selectable_value(&mut self.serialport_baudrate, 115200, "115200");
-                    ui.selectable_value(&mut self.serialport_baudrate, 230400, "230400");
-                    ui.selectable_value(&mut self.serialport_baudrate, 460800, "460800");
-                    ui.selectable_value(&mut self.serialport_baudrate, 576000, "576000");
-                    ui.selectable_value(&mut self.serialport_baudrate, 921600, "921600");
+                    ui.selectable_value(&mut self.serial.baud_rate, 50, "50");
+                    ui.selectable_value(&mut self.serial.baud_rate, 75, "75");
+                    ui.selectable_value(&mut self.serial.baud_rate, 110, "110");
+                    ui.selectable_value(&mut self.serial.baud_rate, 134, "134");
+                    ui.selectable_value(&mut self.serial.baud_rate, 150, "150");
+                    ui.selectable_value(&mut self.serial.baud_rate, 200, "200");
+                    ui.selectable_value(&mut self.serial.baud_rate, 300, "300");
+                    ui.selectable_value(&mut self.serial.baud_rate, 600, "600");
+                    ui.selectable_value(&mut self.serial.baud_rate, 1200, "1200");
+                    ui.selectable_value(&mut self.serial.baud_rate, 1800, "1800");
+                    ui.selectable_value(&mut self.serial.baud_rate, 2400, "2400");
+                    ui.selectable_value(&mut self.serial.baud_rate, 4800, "4800");
+                    ui.selectable_value(&mut self.serial.baud_rate, 9600, "9600");
+                    ui.selectable_value(&mut self.serial.baud_rate, 19200, "19200");
+                    ui.selectable_value(&mut self.serial.baud_rate, 28800, "28800");
+                    ui.selectable_value(&mut self.serial.baud_rate, 38400, "38400");
+                    ui.selectable_value(&mut self.serial.baud_rate, 57600, "57600");
+                    ui.selectable_value(&mut self.serial.baud_rate, 76800, "76800");
+                    ui.selectable_value(&mut self.serial.baud_rate, 115200, "115200");
+                    ui.selectable_value(&mut self.serial.baud_rate, 230400, "230400");
+                    ui.selectable_value(&mut self.serial.baud_rate, 460800, "460800");
+                    ui.selectable_value(&mut self.serial.baud_rate, 576000, "576000");
+                    ui.selectable_value(&mut self.serial.baud_rate, 921600, "921600");
                 });
 
             // data bits
             egui::ComboBox::from_label("Data bits")
-                .selected_text(match self.serialport_databits {
+                .selected_text(match self.serial.data_bits {
                     serialport::DataBits::Five => "5",
                     serialport::DataBits::Six => "6",
                     serialport::DataBits::Seven => "7",
@@ -180,22 +161,18 @@ impl GroundControlApp {
                 })
                 .show_ui(ui, |ui| {
                     ui.selectable_value(
-                        &mut self.serialport_databits,
+                        &mut self.serial.data_bits,
                         serialport::DataBits::Five,
                         "5",
                     );
+                    ui.selectable_value(&mut self.serial.data_bits, serialport::DataBits::Six, "6");
                     ui.selectable_value(
-                        &mut self.serialport_databits,
-                        serialport::DataBits::Six,
-                        "6",
-                    );
-                    ui.selectable_value(
-                        &mut self.serialport_databits,
+                        &mut self.serial.data_bits,
                         serialport::DataBits::Seven,
                         "7",
                     );
                     ui.selectable_value(
-                        &mut self.serialport_databits,
+                        &mut self.serial.data_bits,
                         serialport::DataBits::Eight,
                         "8",
                     );
@@ -203,71 +180,48 @@ impl GroundControlApp {
 
             // parity
             egui::ComboBox::from_label("Parity")
-                .selected_text(match self.serialport_parity {
+                .selected_text(match self.serial.parity {
                     serialport::Parity::Even => "Even",
                     serialport::Parity::Odd => "Odd",
                     serialport::Parity::None => "None",
                 })
                 .show_ui(ui, |ui| {
-                    ui.selectable_value(
-                        &mut self.serialport_parity,
-                        serialport::Parity::None,
-                        "None",
-                    );
-                    ui.selectable_value(
-                        &mut self.serialport_parity,
-                        serialport::Parity::Even,
-                        "Even",
-                    );
-                    ui.selectable_value(
-                        &mut self.serialport_parity,
-                        serialport::Parity::Odd,
-                        "Odd",
-                    );
+                    ui.selectable_value(&mut self.serial.parity, serialport::Parity::None, "None");
+                    ui.selectable_value(&mut self.serial.parity, serialport::Parity::Even, "Even");
+                    ui.selectable_value(&mut self.serial.parity, serialport::Parity::Odd, "Odd");
                 });
 
             // stop bits
             egui::ComboBox::from_label("Stop bits")
-                .selected_text(match self.serialport_stopbits {
+                .selected_text(match self.serial.stop_bits {
                     serialport::StopBits::One => "1",
                     serialport::StopBits::Two => "2",
                 })
                 .show_ui(ui, |ui| {
-                    ui.selectable_value(
-                        &mut self.serialport_stopbits,
-                        serialport::StopBits::One,
-                        "1",
-                    );
-                    ui.selectable_value(
-                        &mut self.serialport_stopbits,
-                        serialport::StopBits::Two,
-                        "2",
-                    );
+                    ui.selectable_value(&mut self.serial.stop_bits, serialport::StopBits::One, "1");
+                    ui.selectable_value(&mut self.serial.stop_bits, serialport::StopBits::Two, "2");
                 });
         });
 
         // status
         ui.label(format!(
             "Status: {}",
-            match self.serialconnection.isconnected() {
-                true => "connected",
-                false => "disconnected",
+            match self.serial.connection_status() {
+                serial_connection::Status::Connected => "Connected",
+                serial_connection::Status::Connecting => "Connecting...",
+                serial_connection::Status::Disconnected => "Disconnected",
+                serial_connection::Status::Disconnecting => "Disconnecting...",
+                serial_connection::Status::Failed => "Failed",
             }
         ));
+        ui.label(format!("Bytes read: {}", self.serial.bytes_read()));
         ui.label("Error rate: 0% (placeholder)");
 
         // connect/disconnect buttons
         ui.horizontal(|ui| {
             ui.add_enabled_ui(connect_isenabled, |ui| {
                 if ui.button("Connect").clicked() {
-                    self.serialconnection.connect(
-                        self.serialport_selectedport.clone(),
-                        self.serialport_baudrate,
-                        self.serialport_databits,
-                        self.serialport_parity,
-                        self.serialport_stopbits,
-                        100,
-                    );
+                    self.serial.connect(self.serial.selected_port.clone());
                 }
             });
 
@@ -297,37 +251,13 @@ impl GroundControlApp {
 
 impl eframe::App for GroundControlApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        ctx.request_repaint();
-
-        // read serialport
-        // TODO: move to another thread
-        match &mut self.serialconnection.reader {
-            Some(r) => {
-                let mut buffer = String::new();
-                let _ = r.read_line(&mut buffer);
-                let _ = buffer.pop(); // remove trailing '\n'
-                self.serialport_messages.push(buffer);
+        // Read all pending data from serial port buffer
+        loop {
+            if let Ok(_) = self.serial.read_byte() {
+            } else {
+                break;
             }
-            None => {}
         }
-
-        // add fake data
-        self.data
-            .altitude
-            .add_point(self.data_t, rand::random::<f64>() * 30000.0);
-        self.data
-            .airpressure
-            .add_point(self.data_t, rand::random::<f64>() * 100.0);
-        self.data
-            .acceleration_x
-            .add_point(self.data_t, rand::random::<f64>() * 100.0);
-        self.data
-            .acceleration_y
-            .add_point(self.data_t, rand::random::<f64>() * 1000.0);
-        self.data
-            .acceleration_z
-            .add_point(self.data_t, rand::random::<f64>() * 100.0);
-        self.data_t += 0.1 / 60.0;
 
         egui::TopBottomPanel::top("menubar").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
@@ -425,12 +355,6 @@ impl eframe::App for GroundControlApp {
                             });
                         });
                         ui.label("placeholder");
-
-                        ui.collapsing("Log", |ui| {
-                            for m in &self.serialport_messages {
-                                ui.label(m);
-                            }
-                        });
                     });
 
                     ui.collapsing("Data", |ui| {
@@ -473,32 +397,5 @@ impl eframe::App for GroundControlApp {
                 ui.label("placeholder");
             }
         });
-    }
-}
-
-impl Default for GroundControlApp {
-    fn default() -> Self {
-        Self {
-            tab_plot: PlotTab::new(),
-            tab_live: LiveTab::new(),
-            tab_trajectory: TrajectoryTab::new(),
-
-            data: Data::new(),
-            data_t: 0.0,
-
-            serialconnection: serial_connection::SerialConnection::new(),
-
-            serialport_knownports: Vec::new(),
-            serialport_selectedport: "".to_string(),
-            serialport_baudrate: 9600,
-            serialport_databits: serialport::DataBits::Eight,
-            serialport_parity: serialport::Parity::None,
-            serialport_stopbits: serialport::StopBits::One,
-
-            serialport_messages: Vec::new(),
-
-            ui_showsidebar: true,
-            ui_selectedtab: AppTab::Plot,
-        }
     }
 }
