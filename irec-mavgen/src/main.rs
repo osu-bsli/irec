22a0d74bf80af77dae2d727ba6daf955e91b30e6
Brian Jia <brian.jia04@gmail.com>
2025-03-30 14:06:51 -0400

    irec-mavgen: Add mavlink, again!!

--- irec-mavgen/src/main.rs
+++ irec-mavgen/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs::File};
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let file = File::open(args[1].clone())?;
+    let file = BufReader::new(file); // Buffering is important for performance
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{:spaces$}+{name}", "", spaces = depth * 2);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{:spaces$}-{name}", "", spaces = depth * 2);
+            }
+            Err(e) => {
+                eprintln!("Error: {e}");
+                break;
+            }
+            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
