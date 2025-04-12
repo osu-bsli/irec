b124766afe8b095d0da9388fd29be08dc4b47b1d
Brian Jia <brian.jia04@gmail.com>
2025-04-12 00:28:24 -0400

    Decrease I2C sensor timeout time. IF SENSORS DO NOT WORK ON THIS COMMIT IT IS A HARDWARE ISSUE.

--- flight-software/Core/Inc/common.h
+++ flight-software/Core/Inc/common.h
@@ -18,7 +18,7 @@ int16_t u8_to_i16(uint8_t lsb, uint8_t msb);
 #define HAL_I2C_Mem_Read_IT_Semaphore(binary_semaphore, hi2c, DevAddress, MemAddress, MemAddSize, pData, Size)  \
     ({                                                                                                          \
         HAL_StatusTypeDef _status = HAL_I2C_Mem_Read_IT(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size); \
-        if (xSemaphoreTake(*binary_semaphore, 100) != true)                                                \
+        if (xSemaphoreTake(*binary_semaphore, 5) != true)                                                       \
         {                                                                                                       \
             SEGGER_RTT_printf(0, SENSOR_NAME ": HAL_I2C_Mem_Read_IT_Semaphore timeout\n");                      \
             _status = HAL_TIMEOUT;                                                                              \
@@ -29,7 +29,7 @@ int16_t u8_to_i16(uint8_t lsb, uint8_t msb);
 #define HAL_I2C_Mem_Write_IT_Semaphore(binary_semaphore, hi2c, DevAddress, MemAddress, MemAddSize, pData, Size)  \
     ({                                                                                                           \
         HAL_StatusTypeDef _status = HAL_I2C_Mem_Write_IT(hi2c, DevAddress, MemAddress, MemAddSize, pData, Size); \
-        if (xSemaphoreTake(*binary_semaphore, 100) != true)                                                 \
+        if (xSemaphoreTake(*binary_semaphore, 5) != true)                                                        \
         {                                                                                                        \
             SEGGER_RTT_printf(0, SENSOR_NAME ": HAL_I2C_Mem_Write_IT_Semaphore timeout\n");                      \
             _status = HAL_TIMEOUT;                                                                               \
