f35fa2a0e82d71e2bbb434d4d70ab364931dcd32
Brian Jia <jia.659@buckeyemail.osu.edu>
2026-05-16 12:28:18 -0400

    Extension shows up in OpenRocket

---
+++ openrocket-plugin-airbrakes/src/AirbrakesProvider.java
@@ -0,0 +1,9 @@
+import info.openrocket.core.plugin.Plugin;
+import info.openrocket.core.simulation.extension.AbstractSimulationExtensionProvider;
+
+@Plugin
+public class AirbrakesProvider extends AbstractSimulationExtensionProvider {
+    public AirbrakesProvider() {
+        super(Airbrakes.class, "Active controls", "BSLI IREC Airbrakes");
+    }
+}
