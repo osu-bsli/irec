96ce5a83901ad57a4eabb596be846ce8d8f2c8ee
Brian Jia <jia.659@buckeyemail.osu.edu>
2026-05-17 12:10:36 -0400

    add telegps data to repo for visualizer, add airbrake slew to physics model

--- flight-software-pico/src/Airbrakes/AB_Deployment.h
+++ flight-software-pico/src/Airbrakes/AB_Deployment.h
@@ -2,11 +2,10 @@
 #include "AB_Struct_Storage.h"
 #include "MathFunctions.h"
 
-#define MASS 21 // kg
+#define MASS 15 // kg
 
 float PredictDeploymentPct(struct apogeeIC ic, float targetApogee, int *out_itersReqd);
-
-float PredictApogee(const struct apogeeIC ic);
+float PredictApogee(const struct apogeeIC ic, const float targetAirbrakeDeployment_pct);
 
 float AB_drag_force(float deployment_pct, float vTotal_mps, float altitude_m);
 float AB_drag_accel(float deployment_pct, float vTotal_mps, float altitude_m);
