3250aa771ec5602c2d1c710f998737af4c3c51a2
gwn <gwn@codeberg.org>
2026-09-02 19:19:20 -0400

    organization

--- airbrakes-gnc-tuning-gui/src/pico-stub/pico.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#ifndef __not_in_flash_func
-#define __not_in_flash_func(func_name) func_name
-#endif
--- airbrakes-gnc-tuning-gui/src/util.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-float r_squared_of_drag_coeff_func_against_theoretical(float ground_level_temp_celcius);
-
